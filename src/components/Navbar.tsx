ec59f2293ed610047bbd77e14aa96840792e18c4
liamasis <liamcalebob@gmail.com>
2023-05-19 21:51:43 +1000

    remove apostrophe

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar: FC<NavbarProps> = ({}) => {
     return <div className='backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between'>
         <div className="container max-w-7xl m-auto w-full flex justify-between items-center">
             <Link href='/' className='text-dark dark:text-white'>
-                Liam O'Brien
+                Liam O&apos;Brien
             </Link>
             <div className="md-hidden">
                 <ThemeToggle />
