765a2b82e9a4655412871f859853516c9bb68803
liamastray <liamcalebob@gmail.com>
2023-05-20 20:29:10 +1000

    add content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,18 @@ import LargeHeading from '@/components/ui/LargeHeading';
 
 export default function Home() {
   return (
-    <main className=' h-screen flex items-center justify-center '>
+    <main className=' h-[calc(100vh-5rem)] flex items-center justify-center '>
         {/* The circle */}
-        <div className="bg-red-700 rounded-full inline-flex items-center justify-center aspect-square w-2/6 absolute"></div>
+        <div className="bg-orange-700 rounded-full inline-flex items-center justify-center aspect-square lg:w-2/6 absolute w-80 opacity-90 "></div>
         
-        <div className="p-40 grid grid-cols-1 z-50">
-          <LargeHeading className="py-5">Creating Clean Code</LargeHeading>
-          <LargeHeading className="py-5">Producing Sleek UI</LargeHeading>
-          <LargeHeading className="py-5">Communicating for Improvement</LargeHeading>
+        <div className=" grid grid-cols-1 lg:grid-cols-6 z-50">
+          <div className="col-span-1"></div>
+          <div className="lg:col-span-2 col-span-1">
+            <LargeHeading className="py-5 text-left">Creating Clean Code</LargeHeading>
+            <LargeHeading className="py-5 text-left">Producing Sleek UI</LargeHeading>
+            <LargeHeading className="py-5 text-left">Communicating for Improvement</LargeHeading>
+          </div>
+          
         </div>
         
     </main>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,9 +10,9 @@ const Navbar: FC<NavbarProps> = ({}) => {
     
     return <div className='backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between'>
         <div className="container max-w-7xl m-auto w-full flex justify-between items-center">
-            <Link href='/' className='text-dark dark:text-white'>
+           {/*  <Link href='/' className='text-dark dark:text-white'>
                 liamo portfolio
-            </Link>
+            </Link> */}
             <div className="md-hidden">
                 <ThemeToggle />
             </div>
