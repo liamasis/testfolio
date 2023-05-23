b970ff0a169f26e5f199b73ced7887bb8d0f52db
liamastray <liamcalebob@gmail.com>
2023-05-23 18:54:54 +1000

    figuring out spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,15 +10,20 @@ export default function Home() {
         {/* The circle */}
         <div className="bg-orange-700 rounded-full inline-flex items-center justify-center aspect-square lg:w-2/6 absolute w-80 opacity-90 "></div>
         
-        <div className=" grid grid-cols-1 lg:grid-cols-6 z-50 grid-flow-col">
-        
+        <div className=" grid grid-rows-10 grid-cols-1 lg:grid-cols-6 z-50 grid-flow-col">
+        <div className="col-span-2 col-start-6 row-span-2 row-start-1 text-white"> 
+          <LargeHeading size={'sm'}>
+            Liam O'Brien
+          </LargeHeading>
+        </div>
           {/* <div className="invisible lg:visible lg:col-span-1"></div> */}
-          <div className="lg:col-start-2 lg:col-span-3 col-span-1">
+          <div className="lg:col-start-2 lg:row-start-3 lg:row-span-4 lg:col-span-3 col-span-1">
             <LargeHeading className="py-5 text-left">Creating Clean Code</LargeHeading>
             <LargeHeading className="py-5 text-left">Producing Sleek UI</LargeHeading>
             <LargeHeading className="py-5 text-left">Communicating for Improvement</LargeHeading>
             
           </div>
+          
         </div>
         
     </main>
