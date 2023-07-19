552e4b342081eb1d2c125c9944ea5bbec2a37367
liamasis <liamcalebob@gmail.com>
2023-07-19 21:00:28 +0200

    responsive changes for 2xl screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
         
       <div className="grid grid-cols-1 xl:grid-cols-6 h-full flex items-center z-50">
         <div className="col-start-1 xl:col-start-2 xl:text-left text-center xl:col-span-3">
-          <LargeHeading size={'lg'} className="py-5">Constructing Clean Code</LargeHeading>
-          <LargeHeading size={'lg'} className="py-5">Creating Sleek UI</LargeHeading>
-          <LargeHeading size={'lg'} className="py-5">Communicating for <br/>Improvement</LargeHeading>
+          <LargeHeading size={'lg'} className="py-4 2xl:py-8">Constructing Clean Code</LargeHeading>
+          <LargeHeading size={'lg'} className="py-4 2xl:py-8">Creating Sleek UI</LargeHeading>
+          <LargeHeading size={'lg'} className="py-4 2xl:py-8">Communicating for <br/>Improvement</LargeHeading>
 
         </div>
-        <div className="col-start-1 col-span-1 xl:col-start-5 xl:col-span-4 h-full w-full flex-grow items-start grid grid-rows-3 xl:grid-rows-4">
-          <div className="col-span-3 xl:col-span-1 xl:row-start-1 xl:row-span-1 h-full w-full flex justify-center items-center ">
+        <div className="col-start-1 col-span-1 xl:col-start-5 xl:col-span-4 h-full w-full flex-grow items-start grid grid-rows-3 xl:grid-rows-4 2xl:grid-rows-6">
+          <div className="col-span-3 xl:col-span-1 xl:row-start-1 2xl:row-start-2 xl:row-span-1 h-full w-full flex justify-center items-center ">
             <div className="min-h-1/2 w-4/5 flex-col justify-center items-center border-4 border-solid rounded-lg dark:border-slate-500 border-slate-800">
               <div className="h-6 border-b-4 dark:border-slate-500 border-slate-800 flex flex-row-reverse gap-1 items-center justify-items-end">
                 <div className="pr-1 flex gap-1">
@@ -25,10 +25,10 @@ export default function Home() {
                   <div className="rounded-full bg-red-400 h-4 w-4 border-2 dark:border-slate-500 border-slate-800"></div>
                 </div>
               </div>
-             <Paragraph className='p-4'>My name is <b className='text-black dark:text-white'>Liam O&apos;Brien</b>, a web developer and software engineer.  </Paragraph>
+             <Paragraph className='p-2'>My name is <b className='text-black dark:text-white'>Liam O&apos;Brien</b>, a web developer and software engineer.  </Paragraph>
             </div>
           </div>
-          <div className="col-span-3 xl:col-span-1 xl:row-start-2 xl:row-span-2 h-full w-full flex justify-center items-center ">
+          <div className="col-span-3 xl:col-span-1 xl:row-start-2 2xl:row-start-3 xl:row-span-2 h-full w-full flex justify-center items-center ">
           <div className="w-5/6 min-h-5/6 flex-col justify-start items-start border-4 border-solid rounded-lg dark:border-slate-500 border-slate-800">
             <div className="h-6 border-b-4 dark:border-500 dark:border-slate-500 border-slate-800 flex flex-row-reverse gap-1 items-center justify-items-end">
               <div className="pr-1 flex gap-1">
@@ -51,7 +51,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="col-start-2 xl:col-span-1 row-span-1 xl:row-start-4 h-full w-full flex justify-center items-center">
+          <div className="col-start-2 xl:col-span-1 row-span-1 xl:row-start-4 2xl:row-start-5 h-full w-full flex justify-center items-center">
             <div className="flex xl:flex-row flex-col items-center justify-center ">
               
               <div className="flex items-center lg:justify-center">
