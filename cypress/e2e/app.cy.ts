0fc60b7531b9687d889ad56545630f6c1fe0143d
liamasis <liamcalebob@gmail.com>
2023-11-13 11:20:32 +0000

    testing suite failing in ci/cd, test.

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -2,5 +2,6 @@ describe('Navigation', () => {
   it('should navigate to the about page', () => {
     // Start from the index page
     cy.visit('http://localhost:3000/')
+    // Testing my cypress
   })
 })
--- src/components/ui/FadeSection.tsx
+++ src/components/ui/FadeSection.tsx
@@ -19,6 +19,7 @@ interface FadeSectionProps {
 const FadeSection: FC<FadeSectionProps> = ({ heading, paragraph, delayTime, link, linkName }) => {
 
     const [isLoading, setIsLoading] = useState(true);
+    const [isHover, setHover] = useState(false);
 
     useEffect(() => {
         // Simulate delay for transition effect after page load
@@ -30,10 +31,18 @@ const FadeSection: FC<FadeSectionProps> = ({ heading, paragraph, delayTime, link
 
     return (
     <div className="grid-span-1 flex justify-center items-center">
-        <div className={`w-80 min-h-48 p-2 ßtransition-opacity duration-1000 ${isLoading ? 'opacity-0' : 'opacity-100'} inline-block border border-transparent hover:outline-solid dark:shadow-stone-500 hover:shadow-lg hover:drop-shadow-lg rounded-lg`}>
+        <div
+        onMouseEnter={() => setHover(true)} 
+        onMouseLeave={() => setHover(false)}
+         className={`w-80 min-h-48 p-2 
+            duration-1000 ${isLoading ? 'opacity-0' : 'opacity-100'} 
+             dark:shadow-stone-500 rounded-lg`}>
             <LargeHeading size={'sm'} className='text-center'>{heading} </LargeHeading>
             <Paragraph className='p-2 dark:text-slate-400'>{paragraph}</Paragraph>
-            <div className="w-full flex justify-center items-center font-bold dark:text-slate-100"><Link className="border px-2 rounded dark:border-slate-800" href={link}>{linkName}</Link>
+            <div className={`w-full flex justify-center items-center font-bold dark:text-slate-100 drop-shadow-lg`}>
+                <a href={link} className={`bg-red border px-8 onhover:bg-redS rounded dark:border-slate-800`}>
+                    {linkName}
+                </a>
             </div>
         </div>
     </div>);
