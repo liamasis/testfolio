c36fc36cdb1705247f2459daa085e9307f06ebf5
liamasis <liamcalebob@gmail.com>
2023-12-15 16:45:17 +0000

    fix: production text size differed

--- src/components/ui/LargeHeading.tsx
+++ src/components/ui/LargeHeading.tsx
@@ -1,12 +1,12 @@
-import { FC, forwardRef } from 'react'
-import { VariantProps, cva } from 'class-variance-authority'
+import { FC, forwardRef } from 'react';
+import { VariantProps, cva } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
-import { Inter, Roboto, Averia_Serif_Libre } from 'next/font/google'
+import { Inter, Roboto, Averia_Serif_Libre } from 'next/font/google';
 
-const inter = Inter({ 
+const inter = Inter({
   weight: ['400', '700'],
   subsets: ['latin'],
- })
+});
 
 const headingVariants = cva(
   'text-black dark:text-white text-center font-extrabold leading-tight tracking-tighter',
@@ -14,7 +14,7 @@ const headingVariants = cva(
     variants: {
       size: {
         default: 'text-4xl md:text-5xl lg:text-6xl',
-        lg: 'bold text-3xl sm:text-3xl md:text-3xl lg:text-5xl 2xl:text-6xl xl:text-left',
+        lg: 'bold text-4xl sm:text-4xl md:text-5xl lg:text-5xl 2xl:text-6xl xl:text-left',
         md: 'text-4xl md:text-4xl lg:text-4xl',
         sm: 'text-lg md:text-xl lg:text-2xl',
       },
@@ -23,25 +23,26 @@ const headingVariants = cva(
       size: 'default',
     },
   }
-)
+);
 
-interface LargeHeadingProps extends React.HTMLAttributes<HTMLHeadingElement>, 
+interface LargeHeadingProps
+  extends React.HTMLAttributes<HTMLHeadingElement>,
     VariantProps<typeof headingVariants> {}
 
+const LargeHeading = forwardRef<HTMLHeadingElement, LargeHeadingProps>(
+  ({ className, size, children, ...props }, ref) => {
+    return (
+      <h2
+        ref={ref}
+        {...props}
+        className={cn(headingVariants({ size, className }), inter.className)}
+      >
+        {children}
+      </h2>
+    );
+  }
+);
 
-const LargeHeading = forwardRef<HTMLHeadingElement, LargeHeadingProps> (
-    ({ className, size, children, ...props }, ref) => {
-      return (
-        <h2
-          ref={ref}
-          {...props}
-          className={cn(headingVariants({ size, className }), inter.className)}>
-          {children}
-        </h2>
-      )
-    }
-  )
-  
-LargeHeading.displayName = 'Paragraph'
+LargeHeading.displayName = 'Paragraph';
 
-export default LargeHeading
+export default LargeHeading;
